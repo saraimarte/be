f4aff8ca5e00f8894aa48b82aa17a2c009a426a1
Sarai Marte <smartepujols@gmail.com>
2025-02-03 01:21:34 -0500

    changed cors

--- src/api.js
+++ src/api.js
@@ -4,23 +4,25 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
-app.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200);
-});
+//  Enable CORS for your frontend domain
+app.use(cors({
+    origin: 'https://fe2003.netlify.app',  
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true
+}));
+
+//  Handle Preflight (OPTIONS) Requests
+app.options('*', cors());
 
 const router = express.Router();
 
-router.get('/' , (req, res)=>{
-    res.json({'msg': 'hello world'});
-})
+router.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://fe2003.netlify.app'); 
+    res.json({ 'msg': 'hello world' });
+});
 
-// Make sure the route is correctly mounted
 app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
