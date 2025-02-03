f95b47ad190d4fcc1f7efb6a0118b9de2ed2eb5c
Sarai Marte <smartepujols@gmail.com>
2025-02-03 03:18:06 -0500

    c

--- src/api.js
+++ src/api.js
@@ -2,19 +2,25 @@ const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
 
-app.use(cors({
-    origin: "*",
-}));
-
 const app = express();
 app.use(express.json());
+
+// Enable CORS for frontend domain
+const corsOptions = {
+    origin: 'https://fe2003.netlify.app',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    optionsSuccessStatus: 204
+};
+app.use(cors(corsOptions));
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ 'msg': 'hello world' });
+    res.json({ msg: 'Hello from Netlify Functions!' });
 });
 
+// Ensure function is correctly mounted
 app.use('/.netlify/functions/api', router);
 
 module.exports.handler = serverless(app);
