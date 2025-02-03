05c0c4d4d140c95d64422e74d9926562185fee9b
Sarai Marte <smartepujols@gmail.com>
2025-02-03 00:57:05 -0500

    changed cors

--- src/api.js
+++ src/api.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
+app.use(cors());
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-    origin: '*', 
-    credentials: true 
-}));
 
+// Set CORS headers manually for all responses
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Change '*' to your frontend domain if needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 
 const router = express.Router();
 
