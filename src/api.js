ec4f247d4e2ee870cb2e2ed2165aeca076ed0085
Sarai Marte <smartepujols@gmail.com>
2025-02-03 01:01:37 -0500

    cors changed

--- src/api.js
+++ src/api.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
-app.use(cors({
-    origin: '*', 
-    credentials: true 
-}));
-
+app.options('*', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
+});
 
 const router = express.Router();
 
