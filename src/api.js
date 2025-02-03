81d7b8830c16272709626b9fba3d5ffaf7df4509
Sarai Marte <smartepujols@gmail.com>
2025-02-03 02:24:50 -0500

    c

--- src/api.js
+++ src/api.js
@@ -4,20 +4,6 @@ const serverless = require('serverless-http');
 const app = express();
 app.use(express.json());
 
-// Middleware to set CORS headers manually
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins, or specify the frontend domain
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        return res.status(204).end(); 
-    }
-
-    next();
-});
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
